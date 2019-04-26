136fbb11ec553ba4fa42b76de6cbc38df32b5d85
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-04-25 20:19:39 -0500

    fourth commit

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,22 +1,32 @@
-<script type="text/javascript">
+
+
+
+
+
+{/* var computer answers: "whatever letter I pick?" */}
+{/* var computer options: ["a"-"z"] (like rps game) or ["zebra"] (word choices) or type all letters ("a", "b"...) */}
+var wins = 0 
+var losses = 0
+var guessesLeft = 9
+var yourGuesses = []
+var answer = ["Bitches", "You Ugly", "fuck shit"];
+var number = Math.floor(Math.random()*answer.length)
+var word = answers[number];
+
+document.onkeyup = function(event) {
+        userText.textContent = event.key;
+      };
+
+      con
+
+
+
+// if (user guess === computer guess);
+
+// if (user guess is not correct minus try)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</script>
